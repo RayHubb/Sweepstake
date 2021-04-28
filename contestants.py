480bae706122dfa7fe841f3cc2e507fa31a91cc6
RayHubb <hubbard_raimon@yahoo.com>
2021-04-28 12:28:25 -0500

    initial commit. I added the Class files for the project

---
+++ contestants.py
@@ -0,0 +1,8 @@
+class Contestant:
+
+    def __init__(self):
+        self.first = first
+        self.last = last
+        self.email = email
+        self.address = address
+        self.registration = reg
---
+++ sweepstake.py
@@ -0,0 +1,2 @@
+class Sweepstake:
+    def __init__(self):
